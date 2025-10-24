04f8dac72f8a80b824dec46c03bdb3e692cc9630
RTN17-coder <none02410@gmail.com>
2025-10-24 09:53:39 -0300

    Add password verification from admin.txt

--- main.c
+++ main.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <curses.h>
 
 #define MAX_NOME 81
 #define MAX_ENDER 201
 #define M_cadastro 100
 #define MAX_PASS 99999
 
+int AbrirArquivo(char pass[]){
+	FILE * arq;
+	
+	arq = fopen("admin.txt","rt"); // aqui ele le o arquivo
+	
+	if(arq == NULL){
+		printf("!!!ERRO!!! Problema ao ler o arquivo"); //caso não tier arquivo
+		return 0;
+	}
+	printf("Digite a senha:");	
+	fgets(pass,MAX_PASS,stdin);  // pede a senha
+	pass[strcspn(pass,"\n")] = 0;  // retira o nulo
+	
+	int passconv = atoi(pass); //  converte a string em char
+	int passcorr;
 
+	if(fscanf(arq, "%d", &passcorr) != 1){
+		printf("!!!Erro!!! arquivo quebrado");
+		fclose(arq);
+		return 0;
+	}
+
+	if(passconv == passcorr){ // aqui ele verifica se a senha digitada é a mesda
+		printf("Certa!");		// do "Banco de dados"
+		return -1;
+	}else{
+		printf("!!!!!-Errada-!!!!!");
+		return 0;
+	}
+
+}
+/*
 int Val_usuario(char pass[]){
 	int a= 'A' * ('['+'b');
 
@@ -24,7 +57,7 @@ int Val_usuario(char pass[]){
 	}else{
 		return 0;
 	}
-}
+}*/
 
 struct Endereco_t {
 	char rua[MAX_ENDER];
@@ -44,17 +77,20 @@ int main() {
 	int i;
 	char pass[MAX_PASS];
 
-	if(Val_usuario(pass) == -1){
+	system("clear");
+	if(AbrirArquivo(pass) == -1){
 		do {
 		printf("\n\n========== Cadastro de Pessoas ============");
 		printf("\n[1] Cadastrar");
 		printf("\n[2] Ver Cadastrados");
 		printf("\n[3] Remover Cadastrado");
+		printf("\n[99] Sair");
 		printf("\nEscolha uma opcao: ");
 		scanf("%d", &opc);
 		getchar();
 
 		if(opc == 1) {
+			system("clear");
 			printf("Digite o Nome que deseja se cadastrado: ");
 			fgets(Cadastro[total_cadastros].Nome,MAX_NOME,stdin);
 
@@ -75,7 +111,7 @@ int main() {
 
 		}
 		if(opc == 2) {
-			int sec = 2;
+			system("clear");
 			if(total_cadastros == 0) {
 				printf("nao possui Cadastros");
 
@@ -88,6 +124,7 @@ int main() {
 		}
 
 		if(opc == 3) {
+			system("clear");
 			printf("\n\n==========- Cadastrados -==========\n\n");
 			if(total_cadastros == 0) {
 				printf("!!ATENCAOO!! NAO POSSUI NENHUM DADO CADASTRADO.");
@@ -106,6 +143,11 @@ int main() {
                 printf("%d. Nome:%s.\n", i+1, Cadastro[i].Nome);
             }
 
+		}
+		if(opc == 99){
+			system("clear");
+			printf("bye bye bro\n");
+			return 0;
 		}
 
 		} while(opc != 0);
