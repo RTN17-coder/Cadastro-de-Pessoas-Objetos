f11e3a3851a88e8e5ab88e8a28140f813c11e1dd
RTN17-coder <none02410@gmail.com>
2025-10-23 21:09:25 -0300

    este é o arquivo principal.

---
+++ main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_NOME 81
+#define MAX_ENDER 201
+#define M_cadastro 100
+
+//Struct() sarve criar um novo tipo de dado personalizado,
+// agrupando variC!veis de diferentes tipos sob um C:nico nome
+
+struct Endereco_t {
+	char rua[MAX_ENDER];
+};
+
+struct Cadastro_t {
+	char Nome[MAX_NOME];
+	struct Endereco_t ender;
+	long Idade;
+	char cpf[12];
+};
+
+int main() {
+	struct Cadastro_t Cadastro[M_cadastro]; //aqui estou chamando o Cadastro e atribuindo um nome de Cadastro com tamanho de [M_cadastro]
+	int total_cadastros = 0;
+	int opc;
+	int i;
+
+	do {
+		printf("\n\n========== Cadastro de Pessoas ============");
+		printf("\n[1] Cadastrar");
+		printf("\n[2] Ver Cadastrados");
+		printf("\n[3] Remover Cadastrado");
+		printf("\nEscolha uma opcao: ");
+		scanf("%d", &opc);
+		getchar();
+
+		if(opc == 1) {
+			printf("Digite o Nome que deseja se cadastrado: ");
+			fgets(Cadastro[total_cadastros].Nome,MAX_NOME,stdin);
+
+			printf("Digite a Idade que deseja cadastrar a, %s: ", Cadastro[total_cadastros].Nome);
+			scanf("%ld", &Cadastro[total_cadastros].Idade);
+			getchar();
+
+			printf("Digite o Endereco de %s: ", Cadastro[total_cadastros].Nome);
+			fgets(Cadastro[total_cadastros].ender.rua,MAX_ENDER,stdin);
+
+			printf("Digite o CPF de %s: ", Cadastro[total_cadastros].Nome);
+			fgets(Cadastro[total_cadastros].cpf,12,stdin);
+			getchar();
+
+			total_cadastros++;
+			// aqui adicina 1 toda vez que repetir ()
+			printf("Pessoa Cadastrada com Sucesso!");
+
+		}
+		if(opc == 2) {
+			int sec = 2;
+			if(total_cadastros == 0) {
+				printf("nao possui Cadastros");
+
+			} else {
+				printf("\n\n==========- Cadastros -==========\n\n");
+				for (int i = 0; i < total_cadastros; i++) {
+					printf("%d. Nome: %s, \nIdade: %ld, \nEndereco: %s, \nCPF: %s. ", i + 1, Cadastro[i].Nome, Cadastro[i].Idade, Cadastro[i].ender.rua, Cadastro[i].cpf);
+				}
+			}
+		}
+
+		if(opc == 3) {
+			printf("\n\n==========- Cadastrados -==========\n\n");
+			if(total_cadastros == 0) {
+				printf("!!ATENCAOO!! NAO POSSUI NENHUM DADO CADASTRADO.");
+			}
+			for(int i = 0; i < total_cadastros; i++) {
+				printf("%d. Nome:%s.\n", i+1, Cadastro[i].Nome);
+			}
+			int opc;
+			printf("==== Digite o Numero que deseja remover ====\n");
+			scanf("%d", &opc);
+			for(int i = opc; i < total_cadastros - 1; i++) {
+				Cadastro[i] = Cadastro[i - 1];
+			}
+			total_cadastros--;
+            for(int i = 0; i < total_cadastros; i++){
+                printf("%d. Nome:%s.\n", i+1, Cadastro[i].Nome);
+            }
+
+		}
+
+	} while(opc != 0);
+
+	return 0;
+}
